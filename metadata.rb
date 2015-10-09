5e7f32a593fb42dc4d6d1b0f2b196bf35a679005
gonzalez <edds718@gmail.com>
2015-10-09 15:18:18 -0400

    adding ihs default

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'rsc_ibm_http_server'
+maintainer       'RightScale Inc'
+maintainer_email 'premium@rightscale.com'
+license          'Apache 2.0'
+description      'Installs/Configures rsc_ibm_http_server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'iim'
+depends 'ihs'
+
+recipe 'rsc_ibm_http_server::default'
